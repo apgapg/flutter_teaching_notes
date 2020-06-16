c7ac64ca74f8b5bbcbf676dbfe427a40b2a91ad0
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-06-16 15:57:38 +0530

    Update project

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "flutter_teaching_notes";
+const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowHeight = 720;
