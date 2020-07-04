bd78587a5741d7f4e4d25f302b6ba8f45b229072
apgapg <apgapg@users.noreply.github.com>
2020-07-04 12:41:53 +0000

    deploy: 5083effcd87583a09cdfd0f2960671dd12406fa2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "0aad347c11806c06a1daff4ee79b2806",
+  "main.dart.js": "96e87907078911508622077741c83d27",
 "favicon.png": "0b08facae49ae6ae314b263c75841a79",
 "manifest.json": "19879b99add80d3ba69c0c5de8c72874",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
