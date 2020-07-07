5121fcad9bde3ab44321c0d546a4746c7b5b002c
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-07-07 08:47:38 +0530

    Wrap topic page with responsive container

--- lib/modules/course/pages/topic_page.dart
+++ lib/modules/course/pages/topic_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_teaching_notes/utils/top_level_utils.dart';
 import 'package:flutter_teaching_notes/widgets/images/my_image.dart';
 import 'package:flutter_teaching_notes/widgets/my_divider.dart';
 import 'package:flutter_teaching_notes/widgets/primary_raised_button.dart';
+import 'package:flutter_teaching_notes/widgets/responsive_container.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -21,56 +22,58 @@ class TopicPage extends StatefulWidget {
 class _TopicPageState extends State<TopicPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 2.0,
-        title: Text(widget.item?.title ?? "NA"),
-      ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (!kIsWeb)
-              HtmlWidget(
-                '''<iframe src='https://player.uacdn.net/lesson-raw/player/v585/player-min.html?uuid=${widget.item.id}&use_imgix=1&autoPlay=false&debug=false'></iframe>''',
-                webView: true,
-              ),
-            if (checkIfNotEmpty(widget.item.video))
-              Container(
-                margin: EdgeInsets.only(
-                  bottom: 8,
-                  top: 8,
+    return ResponsiveContainer(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 2.0,
+          title: Text(widget.item?.title ?? "NA"),
+        ),
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (!kIsWeb)
+                HtmlWidget(
+                  '''<iframe src='https://player.uacdn.net/lesson-raw/player/v585/player-min.html?uuid=${widget.item.id}&use_imgix=1&autoPlay=false&debug=false'></iframe>''',
+                  webView: true,
                 ),
-                child: PrimaryRaisedButton(
-                  icon: Icons.videocam,
-                  color: Colors.green,
-                  text: "UNACADEMY VIDEO",
-                  onTap: () async {
-                    if (await canLaunch(widget.item.video)) {
-                      await launch(widget.item.video);
-                    }
-                  },
+              if (checkIfNotEmpty(widget.item.video))
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 8,
+                    top: 8,
+                  ),
+                  child: PrimaryRaisedButton(
+                    icon: Icons.videocam,
+                    color: Colors.green,
+                    text: "UNACADEMY VIDEO",
+                    onTap: () async {
+                      if (await canLaunch(widget.item.video)) {
+                        await launch(widget.item.video);
+                      }
+                    },
+                  ),
                 ),
-              ),
-            if (checkIfListIsNotEmpty(widget.item.images))
-              for (final image in widget.item.images)
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (isDebugMode)
-                      Text(
+              if (checkIfListIsNotEmpty(widget.item.images))
+                for (final image in widget.item.images)
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      if (isDebugMode)
+                        Text(
+                          image,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      MyImage(
                         image,
-                        style: TextStyle(fontSize: 12),
+                        tapEnabled: true,
                       ),
-                    MyImage(
-                      image,
-                      tapEnabled: true,
-                    ),
-                    MyDivider(),
-                  ],
-                ),
-          ],
+                      MyDivider(),
+                    ],
+                  ),
+            ],
+          ),
         ),
       ),
     );
