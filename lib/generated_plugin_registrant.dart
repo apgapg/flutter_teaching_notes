e1010b4b2c2c67b6ac2bcc47611e2d2c27c4ce01
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-07-11 09:27:35 +0530

    Add mail btn on large screen

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,22 +9,21 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:video_player_web/video_player_web.dart';
 
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
-  FirebaseAnalyticsWeb.registerWith(
-      registry.registrarFor(FirebaseAnalyticsWeb));
+  FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
-  SharedPreferencesPlugin.registerWith(
-      registry.registrarFor(SharedPreferencesPlugin));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   VideoPlayerPlugin.registerWith(registry.registrarFor(VideoPlayerPlugin));
   registry.registerMessageHandler();
--- lib/widgets/responsive_container.dart
+++ lib/widgets/responsive_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_teaching_notes/widgets/primary_raised_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResponsiveContainer extends StatelessWidget {
   final Widget child;
@@ -11,21 +13,80 @@ class ResponsiveContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.topCenter,
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 900),
-        child: Container(
-          child: child,
-          decoration: MediaQuery.of(context).size.width >= 900
-              ? BoxDecoration(
-                  border: Border.symmetric(
-                    horizontal: BorderSide(
-                      color: Colors.grey[200],
-                      width: 4,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (MediaQuery.of(context).size.width >= 1300) Spacer(),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width >= 900
+                    ? 900
+                    : MediaQuery.of(context).size.width),
+            child: Container(
+              child: child,
+              decoration: MediaQuery.of(context).size.width >= 900
+                  ? BoxDecoration(
+                      border: Border.symmetric(
+                        horizontal: BorderSide(
+                          color: Colors.grey[200],
+                          width: 4,
+                        ),
+                      ),
+                    )
+                  : null,
+            ),
+          ),
+          if (MediaQuery.of(context).size.width >= 1300)
+            Expanded(
+              child: Scaffold(
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 150,
+                      child: Image.asset('assets/images/review.png'),
                     ),
-                  ),
-                )
-              : null,
-        ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Have Feedback?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      "Reach us out at our support",
+                      style: TextStyle(),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        PrimaryRaisedButton(
+                          text: " Send Mail ".toUpperCase(),
+                          onTap: () {
+                            launch('mailto:ayushpguptaapg@gmail.com?subject=Feedback'
+                                    '&body=Hello Team,%0A%0AGreetings of the ' +
+                                'day%0A%0A...%0A%0ARegards,%0A---');
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
